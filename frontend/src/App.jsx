ee0ed2161dff208cb25cefbe9b6660848ce8cc69
Shamim Reja <shamimad23@gmail.com>
2026-04-03 01:05:58 +0530

    fix: add frontend as normal folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center" className='text-red-600 font-extrabold'>
+        Hello World
+        <Button>
+          Click me
+        </Button>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
