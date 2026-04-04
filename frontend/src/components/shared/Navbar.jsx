7c5147e4e76e28a919451a5e93529265b8f25e17
Shamim Reja <shamimad23@gmail.com>
2026-04-04 12:51:35 +0530

    haha

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,19 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover.jsx";
+import { Avatar, AvatarImage } from "../ui/avatar.jsx";
+import { Button } from "../ui/button.jsx";
 
 function Navbar() {
   return (
     <div className="bg-white">
-      <div>
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
         <div className="text-2xl font-bold">
           Job <span className="text-[#f83002]">Portal</span>
         </div>
-        <div>
+        <div className="flex items-center gap-12">
           <ul className="flex font-medium gap-5 items-center">
             <li>Home</li>
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" />
+              </Avatar>
+            </PopoverTrigger>
+
+            <PopoverContent className="w-80">
+              <div className="flex gap-4 space-y-2">
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                </Avatar>
+                <div>
+                  <h4 className="font-medium">Ahmed Ahemd</h4>
+                  <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet.</p>
+                </div>
+              </div>
+              <div>
+                <Button variant="link">View Profile</Button>
+                <Button variant="link">Logout</Button>
+
+              </div>
+              
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
     </div>
